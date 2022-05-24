eeeadfafb49ca1ba43d28c4866762a45a20c08fa
Sheri Richardson <shericodes@protonmail.com>
2022-05-24 14:09:30 +0200

    Add global theme to storybook

---
+++ .storybook/decorators.tsx
@@ -0,0 +1,20 @@
+import { ThemeProvider } from 'styled-components'
+import { DecoratorFn } from '@storybook/react'
+
+import { GlobalStyle } from '../src/styles/GlobalStyle'
+// import all themes
+import { darkTheme, lightTheme } from '../src/styles/theme'
+
+const withTheme: DecoratorFn = (StoryFn, context) => {
+  const theme = context.parameters.theme || context.globals.theme
+  // use the global theme value to decide which theme to render
+  const storyTheme = theme === 'dark' ? darkTheme : lightTheme
+  return (
+    <ThemeProvider theme={storyTheme}>
+      <GlobalStyle />
+      <StoryFn />
+    </ThemeProvider>
+  )
+}
+
+export const globalDecorators = [withTheme]
